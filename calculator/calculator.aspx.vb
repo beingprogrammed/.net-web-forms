bda0d3f39b2a5153a555024d66e603371c48571a
Ghanshyam <tghanshyam512@gmail.com>
2024-11-26 11:24:17 +0530

    Update calculator.aspx.vb

--- calculator/calculator.aspx.vb
+++ calculator/calculator.aspx.vb
@@ -1,48 +1,46 @@
-﻿Imports System.Data
+Imports System.Data
 
 Public Class calculator
     Inherits System.Web.UI.Page
 
-    Protected Sub btnNumber_Click(sender As Object, e As EventArgs)
+    Protected Sub btn_Click(sender As Object, e As EventArgs)
         Dim btn As Button = CType(sender, Button)
-        If txtScreen.Text = "0" Then
-            txtScreen.Text = btn.CommandArgument
-        Else
-            txtScreen.Text &= btn.CommandArgument
-        End If
-    End Sub
 
-    Protected Sub btnDot_Click(sender As Object, e As EventArgs)
-        If Not txtScreen.Text.Contains(".") Then
-            txtScreen.Text &= "."
-        End If
-    End Sub
-
-    Protected Sub btnClear_Click(sender As Object, e As EventArgs)
-        txtScreen.Text = "0"
-    End Sub
-
-    Protected Sub btnDelete_Click(sender As Object, e As EventArgs)
-        If txtScreen.Text.Length > 1 Then
-            txtScreen.Text = txtScreen.Text.Substring(0, txtScreen.Text.Length - 1)
-        Else
+        ' Check if the clicked button is a number or an operation
+        If btn.Text = "=" Then
+            ' Handle the equals button (optional, depending on your logic)
+            Try
+                Dim result As Double = EvaluateExpression(txtScreen.Text)
+                txtScreen.Text = result.ToString()
+            Catch ex As Exception
+                txtScreen.Text = "Error"
+            End Try
+        ElseIf btn.Text = "AC" Then
+            ' Handle the clear button
             txtScreen.Text = "0"
+        ElseIf btn.Text = "⌫" Then
+            ' Handle the delete button (backspace)
+            If txtScreen.Text.Length > 1 Then
+                txtScreen.Text = txtScreen.Text.Substring(0, txtScreen.Text.Length - 1)
+            Else
+                txtScreen.Text = "0"
+            End If
+        ElseIf btn.Text = "/" Or btn.Text = "*" Or btn.Text = "+" Or btn.Text = "-" Then
+            ' Handle operations (+, -, *, /)
+            ' Append the operation symbol with a space for readability
+            If txtScreen.Text <> "0" Then
+                txtScreen.Text &= " " & btn.Text & " "
+            End If
+        Else
+            ' Handle number buttons and dot
+            If txtScreen.Text = "0" Then
+                txtScreen.Text = btn.Text
+            Else
+                txtScreen.Text &= btn.Text
+            End If
         End If
     End Sub
 
-    Protected Sub btnOperation_Click(sender As Object, e As EventArgs)
-        Dim btn As Button = CType(sender, Button)
-        txtScreen.Text &= btn.CommandArgument
-    End Sub
-
-    Protected Sub btnEquals_Click(sender As Object, e As EventArgs)
-        Try
-            Dim result As Double = EvaluateExpression(txtScreen.Text)
-            txtScreen.Text = result.ToString()
-        Catch ex As Exception
-            txtScreen.Text = "Error"
-        End Try
-    End Sub
 
     Private Function EvaluateExpression(expression As String) As Double
         Dim table As New DataTable()
